ce366b2cc47177934e4b618ff214123e83c9f533
RobertaBtt <roberta.btt@gmail.com>
2021-03-09 18:44:45 +0100

    Added the Presentation Layer, the hicher level that calls the robot service

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from command import rover
-from command import validator
+from service_layer import services
 
 if __name__ == '__main__':
+    robot_list = []
+    try:
+        upper_right = tuple(input().split(" "))
+        area = services.create_area(int(0), int(0), int(upper_right[0]), int(upper_right[1]))
 
-    print("Write initial coordinates in the form x,y,direction")
-    initial_state_subject = tuple(input().split(","))
-
-    if validator.Validator.subject_is_ok(initial_state_subject):
-        receiver = rover.Rover(initial_state_subject)
-
-        while True:
-            print("Rover is at position ", receiver.get_state())
-            print("Write command:")
-
-            parameters = input()
-
-            if validator.Validator.parameters_are_ok(parameters):
-                receiver.move(parameters)
-
-
-    print("Exiting due to the wrong format")
-
+        if area is None:
+            print("Check area parameters")
+        else:
+            while True:
+                first_list = tuple(input().split(" "))
+                robot = services.create_robot(int(first_list[0]), int(first_list[1]), first_list[2], area)
+                if robot is not None:
+                    instructions = input()
+                    services.send_instructions(robot, instructions)
+                    robot_list.append(robot)
+                else:
+                    print("Robot cannot be deployed, check its coordinates, orientation and area size")
+    except Exception as ex:
+        pass
+    finally:
+        if len(robot_list) > 0:
+            for robot in robot_list:
+                print(robot.position.x, end=" ")
+                print(robot.position.y, end=" ")
+                print(robot.orientation)
 
